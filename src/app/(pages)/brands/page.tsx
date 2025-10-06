34e41bd589bed7b0729f3ec285db30dca0920d96
Mahmoud Ibrahim <Mahmoudibrahim9075@gmail.com>
2025-10-07 07:17:11 +1300

    Update page.tsx

--- src/app/(pages)/brands/page.tsx
+++ src/app/(pages)/brands/page.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 
-interface PageProps {
+type PageProps = {
   params: { brandId: string };
-}
+};
 
 export default function Page({ params }: PageProps) {
   const { brandId } = params;
-
   return (
     <div className="flex flex-col items-center justify-center min-h-screen text-center">
       <h1 className="text-3xl font-bold text-purple-600 mb-4">Brand Details</h1>
